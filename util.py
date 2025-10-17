5c523daaaa36c93bbb57fa9fd577cb6f1792e013
HyungiMin <hyunki8783@tukorea.ac.kr>
2025-10-17 23:52:08 +0900

    기본적 계산 추가

---
+++ util.py
@@ -0,0 +1,23 @@
+from math import *
+from dataclasses import dataclass
+
+@dataclass
+class Vector2:
+    x: float = 0
+    y: float = 0
+
+    def __add__(self, other):
+        return Vector2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector2(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, scalar):
+        return Vector2(self.x * scalar, self.y * scalar)
+
+    def __truediv__(self, scalar):
+        return Vector2(self.x / scalar, self.y / scalar)
+
+    def length(self):
+        return sqrt(self.x ** 2 + self.y ** 2)
+
