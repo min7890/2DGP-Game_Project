34661b01ab5eb6f9c12de94992eee5c376f0ffda
HyungiMin <hyunki8783@tukorea.ac.kr>
2025-10-18 01:09:20 +0900

    scene추가

---
+++ PScene.py
@@ -0,0 +1,17 @@
+class Pscene:
+    def __init__(self):
+        self.elements = {}
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def add_element(self, element):
+        pass
+
+    def remove_element(self, element):
+        pass
+
+
