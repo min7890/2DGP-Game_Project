3f1596cd76b3490605aacc679ccde8fe0424485f
HyungiMin <hyunki8783@tukorea.ac.kr>
2025-11-11 14:12:52 +0900

    player 프레임에 맞춰 움직이도록 walk부분 수정

--- player.py
+++ player.py
@@ -6,6 +6,13 @@ import game_framework
 
 from state_machine import StateMachine
 
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+WALK_SPEED_KMPH = 5.0  # Km / Hour  성인은 평균적으로 한시간에 약 4~5킬로미터 정도 걷는다고함.
+WALK_SPEED_MPM = (WALK_SPEED_KMPH * 1000.0 / 60.0)
+WALK_SPEED_MPS = (WALK_SPEED_MPM / 60.0)
+WALK_SPEED_PPS = (WALK_SPEED_MPS * PIXEL_PER_METER)
+
+
 # 플레이어 클래스
 class Idle:
     def __init__(self, player):
@@ -90,25 +97,23 @@ class Walk:
     def enter(self, e):
         if right_down(e):
             self.player.dir = self.player.face_dir = 1
-            self.player.velocity_x = 200  # 오른쪽으로 이동
         elif left_down(e):
             self.player.dir = self.player.face_dir = -1
-            self.player.velocity_x = -200  # 왼쪽으로 이동
 
     def exit(self, e):
         self.player.velocity_x = 0  # 멈춤
 
     def do(self):
-        self.player.x += self.player.velocity_x * game_framework.frame_time
+        self.player.x += self.player.dir * WALK_SPEED_PPS * game_framework.frame_time
         # 화면 범위 제한
         if self.player.x < 50:
             self.player.x = 50
-        if self.player.x > 750:
-            self.player.x = 750
+        if self.player.x > 1230:
+            self.player.x = 1230
 
     def draw(self):
         scale = 3
-        walk_time = self.player.time * 10 if abs(self.player.velocity_x) > 0 else 0
+        walk_time = self.player.time * 2 * math.pi
 
         # 몸통 그리기 (중심)
         sx, sy, sw, sh = self.player.sprite_body
@@ -131,7 +136,7 @@ class Walk:
 
         # 왼쪽 다리 그리기 (걷기 애니메이션)
         sx, sy, sw, sh = self.player.sprite_leg_l
-        leg_offset_l = math.sin(walk_time) * 4 if abs(self.player.velocity_x) > 0 else 0
+        leg_offset_l = math.sin(walk_time) * 4
         leg_y = self.player.y - 4 * scale + leg_offset_l
         self.player.image.clip_composite_draw(
             sx, sy, sw, sh,
@@ -142,7 +147,7 @@ class Walk:
 
         # 오른쪽 다리 그리기 (걷기 애니메이션)
         sx, sy, sw, sh = self.player.sprite_leg_r
-        leg_offset_r = -math.sin(walk_time) * 4 if abs(self.player.velocity_x) > 0 else 0
+        leg_offset_r = -math.sin(walk_time) * 4
         leg_y = self.player.y - 4 * scale + leg_offset_r
         self.player.image.clip_composite_draw(
             sx, sy, sw, sh,
@@ -153,7 +158,7 @@ class Walk:
 
         # 왼쪽 팔 그리기 (걷기 애니메이션)
         sx, sy, sw, sh = self.player.sprite_arm_l
-        arm_offset_l = math.sin(walk_time + math.pi) * 3 if abs(self.player.velocity_x) > 0 else 0
+        arm_offset_l = math.sin(walk_time + math.pi) * 3
         arm_y = self.player.y - 1 * scale + arm_offset_l
         self.player.image.clip_composite_draw(
             sx, sy, sw, sh,
@@ -164,7 +169,7 @@ class Walk:
 
         # 오른쪽 팔 그리기 (걷기 애니메이션)
         sx, sy, sw, sh = self.player.sprite_arm_r
-        arm_offset_r = -math.sin(walk_time + math.pi) * 3 if abs(self.player.velocity_x) > 0 else 0
+        arm_offset_r = -math.sin(walk_time + math.pi) * 3
         arm_y = self.player.y - 1 * scale + arm_offset_r
         self.player.image.clip_composite_draw(
             sx, sy, sw, sh,
