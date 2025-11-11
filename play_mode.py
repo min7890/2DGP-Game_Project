2a718098dc12f12ec6177aa962c4de2a333e906b
HyungiMin <hyunki8783@tukorea.ac.kr>
2025-11-11 13:24:30 +0900

    수업시간에 배운 내용으로 파일 조정

---
+++ play_mode.py
@@ -0,0 +1,44 @@
+from pico2d import *
+
+import game_world
+
+import game_framework
+from player import Player
+
+player = None
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            player.handle_event(event)
+
+def init():
+    global player
+
+    player = Player()
+    game_world.add_object(player, 1)
+
+
+
+
+def update():
+    game_world.update()
+    game_world.handle_collisions()
+
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def finish():
+    game_world.clear()
+
+def pause(): pass
+def resume(): pass
+
