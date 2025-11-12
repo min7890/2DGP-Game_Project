53614b75967c2567cbfdf4ece0c64f8eb562d6fa
HyungiMin <hyunki8783@tukorea.ac.kr>
2025-11-12 14:56:12 +0900

    1,2,3번 몬스터 클래스 추가

---
+++ monster_01.py
@@ -0,0 +1,21 @@
+from pico2d import *
+import math
+import time
+import game_framework
+import game_world
+from state_machine import StateMachine
+
+
+class Monster_1:
+    def __init__(self):
+        self.image = load_image('monster.png')
+        self.x, self.y = 400, 300
+        self.frame = 0
+
+    def update(self):
+        pass
+    def draw(self):
+        pass
+
+    def handle_collision(self, group, other):
+        pass
