7bf1574cb75e64953f5478d1af0b0cb5ac0c0a96
HyungiMin <hyunki8783@tukorea.ac.kr>
2025-11-17 18:53:03 +0900

    map을 만들때 타일을 가져와서 만들도록 변경

--- map_01.py
+++ map_01.py
@@ -1,23 +1,23 @@
 from pico2d import *
+from tile import Tile_01, Tile_02
 
 class Map_01:
     def __init__(self):
         self.image = load_image('background_01.png')
-        self.tile_image_01 = load_image('tile_01.png')
-        self.tile_image_02 = load_image('tile_02.png')
+        self.tiles_01 = [Tile_01(60+i, 30) for i in range(0, 1280, 120)]
+        self.tile_positions = [(400, 250), (400 + 128, 250), (300, 150)]
 
     def update(self):
         pass
 
     def draw(self):
         self.image.draw(1280 // 2, 720// 2, 1280, 720)
-        for i in range(0, 1280, 120):
-            self.tile_image_01.draw(60 + i, 30)
+        for tile in self.tiles_01:
+            tile.draw()
 
-        self.tile_image_02.draw(400, 250)
-        self.tile_image_02.draw(400+128, 250)
-        self.tile_image_02.draw(300, 150)
-
+        tile_02 = [Tile_02(x, y) for x, y in self.tile_positions]
+        for tile in tile_02:
+            tile.draw()
 
 
     def handle_collision(self, group, other):
