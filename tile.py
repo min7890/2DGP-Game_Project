7eb28a3ebd96274dc84466b7238a0f5c6327f4ba
HyungiMin <hyunki8783@tukorea.ac.kr>
2025-11-17 18:52:49 +0900

    tile 클래스를 추가

---
+++ tile.py
@@ -0,0 +1,38 @@
+from pico2d import *
+
+class Tile_01:
+    def __init__(self, x = 400, y =300):
+        self.tile_image_01 = load_image('tile_01.png')
+        self.x, self.y = x, y
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.tile_image_01.draw(self.x, self.y)
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - 65, self.y - 40, self.x + 65, self.y + 40
+
+    def handle_collision(self, group, other):
+        pass
+
+class Tile_02:
+    def __init__(self, x = 400, y =300):
+        self.tile_image_02 = load_image('tile_02.png')
+        self.x, self.y = x, y
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.tile_image_02.draw(self.x, self.y)
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - 64, self.y - 16, self.x + 64, self.y + 16
+
+
+    def handle_collision(self, group, other):
+        pass
