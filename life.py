2f10d417b174e11f1269767566fe1e8b5a55b580
HyungiMin <hyunki8783@tukorea.ac.kr>
2025-11-18 15:41:22 +0900

    life를 화면에 출력할 수 있도록 추가

---
+++ life.py
@@ -0,0 +1,12 @@
+from pico2d import load_image
+
+class Life:
+    def __init__(self, x = 400, y = 300):
+        self.image = load_image('life.png')
+        self.x, self.y = x, y
+
+    def draw(self):
+        self.image.draw(self.x, self.y, 100, 100)
+
+    def update(self):
+        pass
