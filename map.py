d994be6e23538d74c2700e6e1c93fbd3546b9b1c
HyungiMin <hyunki8783@tukorea.ac.kr>
2025-11-18 13:41:13 +0900

    시작(테스트용 맵)추가

--- map.py
+++ map.py
@@ -20,3 +20,21 @@ class Map_01:
 
     def handle_collision(self, group, other):
         pass
+
+class Map_Start:
+    def __init__(self):
+        self.image = load_image('background_00.png')
+        self.ground_tiles = [Tile_01(60 + i, 30) for i in range(0, 1280, 120)]
+        self.tiles = [Tile_02(x, y) for x, y in
+                      [(400, 250), (400 + 128, 250), (300, 150), (900, 150), (1028, 150), (1156, 150), (690, 350),
+                       (690 + 128, 350)]]
+    def update(self):
+        pass
+    def draw(self):
+        self.image.draw(1280 // 2, 720 // 2, 1280, 720)
+        for tile in self.ground_tiles:
+            tile.draw()
+        for tile in self.tiles:
+            tile.draw()
+    def handle_collision(self, group, other):
+        pass
