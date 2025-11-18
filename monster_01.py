02d9772b005f50a833ef6815660adddc3793cc17
HyungiMin <hyunki8783@tukorea.ac.kr>
2025-11-18 12:48:35 +0900

    1번 몬스터 히트박스 추가

--- monster_01.py
+++ monster_01.py
@@ -26,9 +26,9 @@ class Monster_1:
     def update(self):
         self.frame = (self.frame + FRAME_PER_SECOND * game_framework.frame_time) % 3
         self.x += self.dir * WALK_SPEED_PPS * game_framework.frame_time
-        if (self.x >= 1230):
+        if (self.x >= 570):
             self.dir = self.face_dir = -1
-        elif (self.x <= 50):
+        elif (self.x <= 370):
             self.dir = self.face_dir = 1
 
         pass
@@ -38,7 +38,12 @@ class Monster_1:
             self.image.clip_composite_draw(int(self.frame) * 120, 420, 110, 190, 3.141592, 'v', self.x, self.y, 110 / 2, 190 / 2)
         else:
             self.image.clip_draw(int(self.frame) * 120, 420, 110, 190, self.x, self.y, 110 / 2, 190 / 2)
+
+        draw_rectangle(*self.get_bb())
         pass
+
+    def get_bb(self):
+        return self.x - 25, self.y - 40, self.x + 25, self.y + 45
 
     def handle_collision(self, group, other):
         pass
