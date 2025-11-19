ac71416de23d916dc4e92eec5325cf774e4f2f36
HyungiMin <hyunki8783@tukorea.ac.kr>
2025-11-20 03:14:56 +0900

    테스트맵 약간 수정

--- map.py
+++ map.py
@@ -26,8 +26,7 @@ class Map_Start:
         self.image = load_image('background_00.png')
         self.ground_tiles = [Tile_01(60 + i, 30) for i in range(0, 1280, 120)]
         self.tiles = [Tile_02(x, y) for x, y in
-                      [(400, 250), (400 + 128, 250), (300, 150), (900, 150), (1028, 150), (1156, 150), (690, 350),
-                       (690 + 128, 350)]]
+                      [(400, 250), (400 + 128, 250), (300, 150)]]
     def update(self):
         pass
     def draw(self):
