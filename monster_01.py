2578eec432d5ccb0bad4992eb010fd78b7226a72
HyungiMin <hyunki8783@tukorea.ac.kr>
2025-12-04 20:47:00 +0900

    해골몬스터가 플레이어를 따라오고, 바운딩 박스 충돌 시 플레이어를 공격

--- monster_01.py
+++ monster_01.py
@@ -4,9 +4,10 @@ import time
 import game_framework
 import game_world
 from state_machine import StateMachine
+from behavior_tree import BehaviorTree, Action, Sequence, Condition, Selector
 import common
 
-PIXEL_PER_METER = (10.0 / 0.2) # 10pixel = 10cm = 0.1m
+PIXEL_PER_METER = (10.0 / 0.3) # 10pixel = 10cm = 0.1m
 WALK_SPEED_KMPH = 10.0 # 10km/h
 WALK_SPEED_MPM = (WALK_SPEED_KMPH * 1000.0 / 60.0)
 WALK_SPEED_MPS = (WALK_SPEED_MPM / 60.0)
@@ -17,11 +18,6 @@ ACTION_PER_TIME = 1.0 / TIME_PER_SECOND
 FRAMES_PER_ACTION = 3
 FRAME_PER_SECOND = FRAMES_PER_ACTION * ACTION_PER_TIME
 
-TIME_PER_SECOND_atk = 1.0
-ACTION_PER_TIME_atk = 1.0 / TIME_PER_SECOND_atk
-FRAMES_PER_ACTION_atk = 6
-FRAME_PER_SECOND_atk = FRAMES_PER_ACTION_atk * ACTION_PER_TIME_atk
-
 class Monster_1:
     def __init__(self, x = 400, y =300):
         self.image = load_image('monster.png')
@@ -31,9 +27,17 @@ class Monster_1:
         self.frame = 0
         self.dir = self.face_dir = 1
 
+        self.tx, self.ty = 400, 300
+        self.state = 'Idle'
+
+        self.build_behavior_tree()
+
+        self.loc_no = 0
+        self.patrol_locations = [(370, 300), (570, 300)]
+
     def update(self):
         if self.is_atk:
-            self.frame = (self.frame + FRAME_PER_SECOND_atk * game_framework.frame_time) % 6
+            self.frame = (self.frame + FRAME_PER_SECOND * game_framework.frame_time) % 6
             if common.player.x <= self.x:
                 self.dir = self.face_dir = -1
             else:
@@ -41,24 +45,25 @@ class Monster_1:
             self.is_atk = False
         else:
             self.frame = (self.frame + FRAME_PER_SECOND * game_framework.frame_time) % 3
-            self.x += self.dir * WALK_SPEED_PPS * game_framework.frame_time
-            if (self.x >= 570):
-                self.dir = self.face_dir = -1
-            elif (self.x <= 370):
-                self.dir = self.face_dir = 1
+            # self.x += self.dir * WALK_SPEED_PPS * game_framework.frame_time
+            # if (self.x >= 570):
+            #     self.dir = self.face_dir = -1
+            # elif (self.x <= 370):
+            #     self.dir = self.face_dir = 1
 
         # print(self.is_atk)
+
+        self.bt.run()
 
         pass
     def draw(self):
         #atk
         if self.is_atk:
-            print('atk')
             if self.face_dir == 1:
                 # self.image.clip_draw(120, 420, 110, 190, 300, 400)
-                self.atk_image.clip_composite_draw(int(self.frame) * 95, 0, 95, 190, 3.141592, 'v', 400, 300, 110 / 2, 190 / 2)
+                self.atk_image.clip_composite_draw(int(self.frame) * 95, 0, 95, 190, 3.141592, 'v', self.x, self.y, 110 / 2, 190 / 2)
             else:
-                self.atk_image.clip_draw(int(self.frame) * 95, 0, 95, 190, 400, 300, 110 / 2, 190 / 2)
+                self.atk_image.clip_draw(int(self.frame) * 95, 0, 95, 190, self.x, self.y, 110 / 2, 190 / 2)
             pass
         #walk
         else:
@@ -70,7 +75,8 @@ class Monster_1:
                 self.image.clip_draw(int(self.frame) * 120, 420, 110, 190, self.x, self.y, 110 / 2, 190 / 2)
 
         draw_rectangle(*self.get_bb())
-        pass
+        draw_circle(self.x, self.y, int(5 * PIXEL_PER_METER), 255, 255, 0)
+
 
     def get_bb(self):
         return self.x - 25, self.y - 40, self.x + 25, self.y + 45
@@ -84,5 +90,68 @@ class Monster_1:
 
     # def handle_detection_collision(self, group, other):
     #     pass
+
+    def set_target_location(self, x=None, y=None):
+        self.tx, self.ty = x, y
+        return BehaviorTree.SUCCESS
+
+    def distance_less_than(self, x1, y1, x2, y2, r):
+        distance2 = (x2 - x1) ** 2 + (y2 - y1) ** 2
+        return distance2 < (r * PIXEL_PER_METER) ** 2
+
+    def move_little_to(self, tx, ty):
+        if tx - self.x < 10:
+            self.dir = -1
+        else:
+            self.dir = 1
+        distance = WALK_SPEED_PPS * game_framework.frame_time
+        self.x += distance * self.dir
+
+    def move_to(self, r=0.5):
+        self.state = 'Walk'
+        self.move_little_to(self.tx, self.ty)
+
+        if self.distance_less_than(self.x, self.y, self.tx, self.ty, r):
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.RUNNING
+
+    def set_random_location(self):
+        pass
+
+    def if_player_nearby(self, distance):
+        if self.distance_less_than(common.player.x, common.player.y, self.x, self.y, distance):
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.FAIL
+
+    def move_to_player(self, r = 0.5):
+
+        self.state = 'Walk'
+        self.move_little_to(common.player.x, common.player.y)
+
+        if self.distance_less_than(common.player.x, common.player.y, self.x, self.y, r):
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.RUNNING
+
+    def get_patrol_location(self):
+        self.tx, self.ty = self.patrol_locations[self.loc_no]
+        self.loc_no = (self.loc_no + 1) % len(self.patrol_locations)
+        return BehaviorTree.SUCCESS
+
+    def build_behavior_tree(self):
+        a1 = Action('목표 지점으로 이동', self.move_to)
+
+        c1 = Condition('플레이어가 근처에 있는가?', self.if_player_nearby, 5)
+        a2 = Action('플레이어 추적', self.move_to_player)
+        chase_if_player_nearby = Sequence('플레이어가 근처에 있으면 추적', c1, a2)
+
+        a3 = Action('다음 순찰 위치를 가져오기', self.get_patrol_location)
+        patrol = Sequence('순찰', a3, a1)
+
+        root = chase_or_patrol = Selector('플레이어가 가까이 있으면 추적하고, 아니면 순찰', chase_if_player_nearby, patrol)
+
+        self.bt = BehaviorTree(root)
 
 
