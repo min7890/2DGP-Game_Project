0a8a16928fe5229c246e21aafd7178ad7c43f980
HyungiMin <hyunki8783@tukorea.ac.kr>
2025-12-07 00:11:46 +0900

    보스맵 기본적인 틀 구성

---
+++ monster_boss.py
@@ -0,0 +1,184 @@
+from pico2d import *
+import math
+import time
+import game_framework
+import game_world
+from state_machine import StateMachine
+
+from behavior_tree import BehaviorTree, Action, Sequence, Condition, Selector
+import common
+import random
+from item import Item
+
+PIXEL_PER_METER = (10.0 / 0.2)
+FLY_SPEED_KMPH = 10.0 # 10km/h
+FLY_SPEED_MPM = (FLY_SPEED_KMPH * 1000.0 / 60.0)
+FLY_SPEED_MPS = (FLY_SPEED_MPM / 60.0)
+FLY_SPEED_PPS = (FLY_SPEED_MPS * PIXEL_PER_METER)
+
+TIME_PER_SECOND = 0.7
+ACTION_PER_TIME = 1.0 / TIME_PER_SECOND
+FRAMES_PER_ACTION = 5
+FRAME_PER_SECOND = FRAMES_PER_ACTION * ACTION_PER_TIME
+
+
+class Monster_boss:
+    def __init__(self, x = 400, y =300):
+        self.image = load_image('boss.png')
+        self.x, self.y = x, y
+        self.frame = 0
+        self.dir = 0.0
+        self.face_dir = 1
+        self.tx, self.ty = 400, 300
+
+        self.life = 4
+        self.is_atk = False
+        self.det = False
+
+        self.build_behavior_tree()
+
+    def update(self):
+        self.det = False
+        self.is_atk = False
+
+        # self.bt.run()
+
+    def draw(self):
+        self.image.clip_draw(0, 0, 230, 380, self.x, self.y)
+
+        draw_rectangle(*self.get_bb())
+        draw_circle(self.x, self.y, int(2 * PIXEL_PER_METER), 255, 255, 0)
+
+    def get_bb(self):
+        return self.x - 110, self.y - 200, self.x + 120, self.y + 180
+
+    def handle_collision(self, group, other):
+        pass
+
+
+    def set_target_location(self, x=None, y=None):
+        self.tx, self.ty = x, y
+        return BehaviorTree.SUCCESS
+
+    def distance_less_than(self, x1, y1, x2, y2, r):
+        distance2 = (x2 - x1) ** 2 + (y2 - y1) ** 2
+        return distance2 < (r * PIXEL_PER_METER) ** 2
+
+
+    def move_little_to(self, tx, ty):
+        #각도 구하기
+        self.dir = math.atan2(ty - self.y, tx - self.x)
+
+        if tx - self.x < 10:
+            self.face_dir = -1
+        else:
+            self.face_dir = 1
+        #거리 구하기
+        distance = FLY_SPEED_PPS * game_framework.frame_time
+        if self.det:
+            if abs(tx - self.x) > 20:
+                self.x += distance * math.cos(self.dir)
+            self.y += distance * math.sin(self.dir)
+        else:
+            self.x += distance * math.cos(self.dir)
+            self.y += distance * math.sin(self.dir)
+
+    def move_to(self, r=0.5):
+        self.state = 'Fly'
+        self.move_little_to(self.tx, self.ty)
+        # 목표 지점에 거의 도착했으면 성공 리턴
+        if self.distance_less_than(self.x, self.y, self.tx, self.ty, r):
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.RUNNING
+
+    def set_random_location(self):
+        self.tx = random.randint(100, 1180)
+        self.ty = random.randint(100, 620)
+        return BehaviorTree.SUCCESS
+
+    def if_player_nearby(self, distance):
+        if self.distance_less_than(common.player.x, common.player.y, self.x, self.y, distance):
+            self.det = True
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.FAIL
+
+    def move_to_player(self, r=0.5):
+        self.state = 'Walk'
+        self.move_little_to(common.player.x, common.player.y)
+        if self.distance_less_than(common.player.x, common.player.y, self.x, self.y, r):
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.RUNNING
+
+    def build_behavior_tree(self):
+
+        a1 = Action('목표 지점으로 이동', self.move_to)
+
+        a2 = Action('랜덤 위치 설정', self.set_random_location)
+        wander = Sequence('배회', a2, a1)
+
+        c1 = Condition('플레이어가 근처에 있는가?', self.if_player_nearby, 2)
+        a3 = Action('플레이어 추적', self.move_to_player)
+        chase_if_player_nearby = Sequence('소년이 근처에 있으면 추적', c1, a3)
+
+        root = chase_or_wander = Selector('소년이 가까이 있으면추적하고, 아니면 배회', chase_if_player_nearby, wander)
+
+        self.bt = BehaviorTree(root)
+
+class Monster_boss_left_hand:
+    def __init__(self):
+        self.image = load_image('boss.png')
+        self.x, self.y = common.monster_boss.x - 100, common.monster_boss.y - 30
+
+        self.dir = 0.0
+        self.tx, self.ty = 400, 300
+
+        self.life = 4
+        self.is_atk = False
+        self.det = False
+
+    def update(self):
+        self.det = False
+        self.is_atk = False
+
+    def draw(self):
+        self.image.clip_draw(260, 15, 75, 70, self.x, self.y)
+
+        draw_rectangle(*self.get_bb())
+        draw_circle(self.x, self.y, int(2 * PIXEL_PER_METER), 255, 255, 0)
+
+    def get_bb(self):
+        return self.x - 40, self.y - 40, self.x + 40, self.y + 25
+
+    def handle_collision(self, group, other):
+        pass
+
+class Monster_boss_right_hand:
+    def __init__(self):
+        self.image = load_image('boss.png')
+        self.x, self.y = common.monster_boss.x + 100, common.monster_boss.y - 30
+
+        self.dir = 0.0
+        self.tx, self.ty = 400, 300
+
+        self.life = 4
+        self.is_atk = False
+        self.det = False
+
+    def update(self):
+        self.det = False
+        self.is_atk = False
+
+    def draw(self):
+        self.image.clip_draw(340, 15, 75, 70, self.x, self.y)
+
+        draw_rectangle(*self.get_bb())
+        draw_circle(self.x, self.y, int(2 * PIXEL_PER_METER), 255, 255, 0)
+
+    def get_bb(self):
+        return self.x - 40, self.y - 40, self.x + 30, self.y + 25
+
+    def handle_collision(self, group, other):
+        pass
---
+++ stage_boss.py
@@ -0,0 +1,147 @@
+from pico2d import *
+
+import game_world
+
+import game_framework
+from player import Player
+from monster_01 import Monster_1
+from monster_02 import Monster_2
+from monster_03 import Monster_3
+from monster_boss import Monster_boss, Monster_boss_left_hand, Monster_boss_right_hand
+
+from life import Life
+
+from map import Map_boss
+import pinput
+import stage_00
+import stage_01
+
+import common
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if common.player.life == 0:
+            game_framework.change_mode(stage_00)
+
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_f and common.player.isInPortal:
+            game_framework.change_mode(stage_00)
+        else:
+            pinput.update_key_state(event)  # 키 상태 업데이트
+            common.player.handle_event(event)
+
+def init():
+    global player
+
+    common.player = Player()
+    if common.player is not None:
+        common.player.life = stage_01.prev_stage_life()
+    game_world.add_object(common.player, 1)
+
+    game_world.add_collision_pair('item:player', None, common.player)
+
+    common.monster_boss = Monster_boss(650, 400)
+    game_world.add_object(common.monster_boss, 1)
+
+    monster_boss_left_hand = Monster_boss_left_hand()
+    game_world.add_object(monster_boss_left_hand, 2)
+
+    monster_boss_left_hand = Monster_boss_right_hand()
+    game_world.add_object(monster_boss_left_hand, 2)
+
+    # monster_1 = [Monster_1(x, 120) for x in (400, 1050)]
+    #
+    # for monster in monster_1:
+    #     game_world.add_object(monster, 1)
+    # game_world.add_collision_pair('monster_1:player', None, common.player)
+    # for monster in monster_1:
+    #     # 플레이어 충돌
+    #     game_world.add_collision_pair('monster_1:player', monster, None)
+    #     # 몬스터1, 원거리공격 충돌
+    #     game_world.add_collision_pair('monster_1:fire', monster, None)
+    #     # 몬스터1, 타일 충돌
+    #     game_world.add_collision_pair('tile:monster_1', None, monster)
+    #     # 몬스터, 검 충돌
+    #     game_world.add_collision_pair('monster:sword', monster, None)
+
+
+    # # monster_1 = [Monster_2(x, y) for x, y in [(400, 285), (600, 88), (900, 185)]]
+    # monster_2 = [Monster_2(x, y) for x, y in [(230, 430), (550, 330), (1000, 430), (818, 380)]]
+    # for monster in monster_2:
+    #     game_world.add_object(monster, 1)
+    #
+    #     game_world.add_collision_pair('monster_2:player', monster, None)
+    #     game_world.add_collision_pair('monster_2:fire', monster, None)
+    #     game_world.add_collision_pair('tile:monster_2', None, monster)
+    #     game_world.add_collision_pair('monster:sword', monster, None)
+    #
+    # game_world.add_collision_pair('monster_2:player', None, common.player)
+    # game_world.add_collision_pair('monster_3:player', None, common.player)
+    #
+    # monster_3 = [Monster_3(200, 400), Monster_3(800, 150)]
+    # for monster in monster_3:
+    #     game_world.add_object(monster, 1)
+    #     # 플레이어 충돌
+    #     game_world.add_collision_pair('monster_3:player', monster, None)
+    #     # 몬스터3, 원거리공격 충돌
+    #     game_world.add_collision_pair('monster_3:fire', monster, None)
+    #     game_world.add_collision_pair('monster:sword', monster, None)
+    # game_world.add_collision_pair('monster_3:player', None, common.player)
+
+
+    global map
+    common.map = Map_boss()
+    game_world.add_object(common.map, 0)
+    #플레이어-타일
+    game_world.add_collision_pair('tile:player', None, common.player)
+
+    for tile in common.map.tiles:
+        game_world.add_collision_pair('tile:player', tile, None)
+
+        game_world.add_collision_pair('tile:monster_2', tile, None)
+
+        game_world.add_collision_pair('tile:item', tile, None)
+
+    game_world.add_collision_pair('portal:player', None, common.player)
+
+
+
+
+
+
+
+
+
+def update():
+    game_world.update()
+    game_world.handle_collisions()
+    # game_world.handle_detection_collisions()
+
+    current_life_objects = [obj for obj in game_world.world[2] if isinstance(obj, Life)]
+    if len(current_life_objects) != common.player.life:
+        # 기존 Life 객체 모두 제거
+        for obj in current_life_objects:
+            game_world.remove_object(obj)
+        # 새 Life 객체 생성
+        lives = [Life(35 + x * 60, 690) for x in range(common.player.life)]
+        for life in lives:
+            game_world.add_object(life, 2)
+
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def finish():
+    game_world.clear()
+
+def pause(): pass
+def resume(): pass
+
+def get_map():
+    return map
