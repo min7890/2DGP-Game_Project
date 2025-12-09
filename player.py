e3bebfd621a1e35f91ae92d13dd5903a9bce2a0d
HyungiMin <hyunki8783@tukorea.ac.kr>
2025-12-10 01:50:55 +0900

    플레이어 피격 시 x축 기준으로 뒤로 밀리도록

--- player.py
+++ player.py
@@ -1211,6 +1211,10 @@ class Player:
                 self.candidate_grounds.append(top + 18)
         if group in ('monster_1:player', 'monster_2:player', 'monster_3:player', 'monster_boss:player', 'monster_boss_left:player', 'monster_boss_right:player'):
             if self.life > 0 and not self.islife_down:
+                if self.x <=  other.x:
+                    self.x -= 20
+                elif self.x > other.x:
+                    self.x += 20
                 self.life -= 1
                 self.islife_down = True
                 self.life_notdown_timer = get_time()
