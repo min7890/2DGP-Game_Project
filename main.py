eea268eec0887f907e065105f3724ea698fc8bfa
HyungiMin <hyunki8783@tukorea.ac.kr>
2025-12-10 19:39:06 +0900

    다시 시작모드를 타이틀모드로

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pico2d import *
 import game_framework
-# import title_mode as start_mode
-import stage_boss as start_mode
+import title_mode as start_mode
+# import stage_boss as start_mode
 
 #플레이어는 1 픽셀당 3cm
 #w = 1280픽셀 * 3cm = 3840cm = 38.4m
